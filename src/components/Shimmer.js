dd2ee3ea0824bf6e487fefbaeda920bc621f7b44
Prajvals1211 <121630883+Prajvals1211@users.noreply.github.com>
2023-10-15 00:00:58 +0530

    Update Shimmer.js

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,25 +2,49 @@ import React from "react";
 
 export const Shimmer = () => {
   return (
-    <div>
-      {Array(10)
-        .fill("")
-        .map((e, i) => {
-          <div key={i} className="w-52 h-72 m-5 bg-gray-500"></div>;
-        })}
+    <>
+      <div className="flex flex-wrap flex-row">
+        {Array(50)
+          .fill("")
+          .map((e, i) => {
+            return (
+              <div className="">
+                <div
+                  key={i}
+                  className="w-[295px] p-2 m-2 h-44 bg-gray-200 rounded flex flex-wrap"
+                ></div>
+                <div>
+                  <Shimmer1 />
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
+};
+
+const Shimmer1 = () => {
+  return (
+    <div className="flex flex-wrap flex-col">
+      <div className="w-[295px]  p-2 m-2  bg-gray-200 h-8 rounded"></div>
+      <div className="w-20  p-2 m-2  bg-gray-200 h-5 rounded"></div>
+      <div className="flex justify-between">
+        <div className="w-20  p-2 m-2  bg-gray-200 h-6 rounded"></div>
+        <div className="w-20  p-2 m-2  bg-gray-200 h-6 rounded"></div>
+      </div>
     </div>
   );
 };
 
-
-export const SuggestShimmer = ()=>{
-    return(
-        <div>
-            {Array(10).fill('').map((e,i)=>{
-                <div className="w-32 h-14 m-2 bg-black" key={i}></div>
-            })}
-        </div>
-    )
-}
-
-
+export const SuggestShimmer = () => {
+  return (
+    <div>
+      {Array(25)
+        .fill("")
+        .map((e, i) => {
+          return <div className="w-[350px] h-20 m-2 bg-gray-200" key={i}></div>;
+        })}
+    </div>
+  );
+};
