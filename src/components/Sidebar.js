43cad55396fe11f273c302963527106821a897f0
Prajvals1211 <121630883+Prajvals1211@users.noreply.github.com>
2023-10-17 13:28:25 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,12 +3,13 @@ import { AiFillHome, AiOutlineFlag, AiFillFire } from "react-icons/ai";
 import { BsBroadcast } from "react-icons/bs";
 import { CgMusicNote } from "react-icons/cg";
 import { FiFilm } from "react-icons/fi";
-import {  GiDiamondTrophy } from "react-icons/gi";
+import { GiDiamondTrophy } from "react-icons/gi";
 import { IoGameControllerSharp } from "react-icons/io5";
 import { FiSettings } from "react-icons/fi";
 import { Link, useLocation, useSearchParams } from "react-router-dom";
 
-const SidebarVisibilty = ({ icons, visibilty }) => {
+const SidebarVisibilty = ({ icons, visibilty,Theme }) => {
+  
   return (
     <>
       {!icons && (
@@ -18,6 +19,10 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
               <li
                 className={`p-1 m-2 rounded-lg hover:bg-gray-100 flex text-md mr-5 ${
                   visibilty === "home" ? "bg-gray-100" : ""
+                } ${
+                  !Theme &&
+                  visibilty !== "home" &&
+                  "text-white hover:text-black"
                 }`}
               >
                 <AiFillHome
@@ -25,13 +30,17 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
                     margin: "2px",
                   }}
                 />
-                <span className="mt-[-5px] ml-2 p-1"> Home</span>
+                <span className={`mt-[-5px] ml-2 p-1 `}> Home</span>
               </li>
             </Link>
             <Link to="/?v=live">
               <li
                 className={`p-1 m-2 rounded-lg hover:bg-gray-100 flex text-md mr-5 ${
                   visibilty === "live" ? "bg-gray-100" : ""
+                } ${
+                  !Theme &&
+                  visibilty !== "live" &&
+                  "text-white hover:text-black"
                 }`}
               >
                 <BsBroadcast
@@ -46,6 +55,8 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
               <li
                 className={`p-1 m-2 rounded-lg hover:bg-gray-100 flex text-md mr-5 ${
                   visibilty === "10" ? "bg-gray-100" : ""
+                } ${
+                  !Theme && visibilty !== "10" && "text-white hover:text-black"
                 }`}
               >
                 <CgMusicNote
@@ -60,6 +71,8 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
               <li
                 className={`p-1 m-2 rounded-lg hover:bg-gray-100 flex text-md mr-5 ${
                   visibilty === "1" ? "bg-gray-100" : ""
+                } ${
+                  !Theme && visibilty !== "1" && "text-white hover:text-black"
                 }`}
               >
                 <FiFilm
@@ -74,6 +87,8 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
               <li
                 className={`p-1 m-2 rounded-lg hover:bg-gray-100 flex text-md mr-5 ${
                   visibilty === "17" ? "bg-gray-100" : ""
+                } ${
+                  !Theme && visibilty !== "17" && "text-white hover:text-black"
                 }`}
               >
                 <GiDiamondTrophy
@@ -88,6 +103,8 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
               <li
                 className={`p-1 m-2 rounded-lg hover:bg-gray-100 flex text-md mr-5 ${
                   visibilty === "20" ? "bg-gray-100" : ""
+                } ${
+                  !Theme && visibilty !== "20" && "text-white hover:text-black"
                 }`}
               >
                 <IoGameControllerSharp
@@ -102,6 +119,8 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
               <li
                 className={`p-1 m-2 rounded-lg hover:bg-gray-100 flex text-md mr-5 ${
                   visibilty === "24" ? "bg-gray-100" : ""
+                } ${
+                  !Theme && visibilty !== "24" && "text-white hover:text-black"
                 }`}
               >
                 <AiFillFire
@@ -115,7 +134,11 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
           </ul>
           <div className=" m-2 border border-b-gray-400 "></div>
           <ul className="">
-            <li className="p-1 py-3 m-2 rounded-lg hover:bg-gray-100 flex text-md mr-5">
+            <li
+              className={`p-1 py-3 m-2 rounded-lg hover:bg-gray-100 flex text-md mr-5  ${
+                !Theme && "text-white hover:text-black"
+              }`}
+            >
               <FiSettings
                 style={{
                   margin: "2px",
@@ -123,7 +146,11 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
               />
               <span className="mt-[-6px] ml-2 p-1">Settings</span>
             </li>
-            <li className="p-1 w-48 m-2 rounded-lg hover:bg-gray-100 flex ">
+            <li
+              className={`p-1 w-48 m-2 rounded-lg hover:bg-gray-100 flex ${
+                !Theme && "text-white hover:text-black"
+              }`}
+            >
               <AiOutlineFlag
                 style={{
                   margin: "2px",
@@ -143,6 +170,10 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
               <li
                 className={`p-1 m-2 rounded-lg hover:bg-gray-100 w-10 ${
                   visibilty === "home" ? "bg-gray-100" : ""
+                } ${
+                  !Theme &&
+                  visibilty !== "home" &&
+                  "text-white hover:text-black"
                 }`}
               >
                 <AiFillHome
@@ -158,6 +189,10 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
               <li
                 className={`p-1 m-2 rounded-lg hover:bg-gray-100 w-10 ${
                   visibilty === "live" ? "bg-gray-100" : ""
+                } ${
+                  !Theme &&
+                  visibilty !== "live" &&
+                  "text-white hover:text-black"
                 }`}
               >
                 <BsBroadcast
@@ -172,6 +207,8 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
               <li
                 className={`p-1 m-2 rounded-lg hover:bg-gray-100 w-10 ${
                   visibilty === "10" ? "bg-gray-100" : ""
+                } ${
+                  !Theme && visibilty !== "10" && "text-white hover:text-black"
                 }`}
               >
                 <CgMusicNote
@@ -186,6 +223,8 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
               <li
                 className={`p-1 m-2 rounded-lg hover:bg-gray-100 w-10 ${
                   visibilty === "1" ? "bg-gray-100" : ""
+                } ${
+                  !Theme && visibilty !== "1" && "text-white hover:text-black"
                 }`}
               >
                 <FiFilm
@@ -200,6 +239,8 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
               <li
                 className={`p-1 m-2 rounded-lg hover:bg-gray-100 w-10 ${
                   visibilty === "17" ? "bg-gray-100" : ""
+                } ${
+                  !Theme && visibilty !== "17" && "text-white hover:text-black"
                 }`}
               >
                 <GiDiamondTrophy
@@ -214,6 +255,8 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
               <li
                 className={`p-1 m-2 rounded-lg hover:bg-gray-100 w-10 ${
                   visibilty === "20" ? "bg-gray-100" : ""
+                }${
+                  !Theme && visibilty !== "20" && "text-white hover:text-black"
                 }`}
               >
                 <IoGameControllerSharp
@@ -228,6 +271,8 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
               <li
                 className={`p-1 m-2 rounded-lg hover:bg-gray-100 w-10 ${
                   visibilty === "24" ? "bg-gray-100" : ""
+                }${
+                  !Theme && visibilty !== "24" ? "text-white hover:text-black" : ""
                 }`}
               >
                 <AiFillFire
@@ -238,11 +283,14 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
                 />
               </li>
             </Link>
-           
           </ul>
           <div className="w-10 m-2 border border-b-gray-400 "></div>
           <ul className="">
-            <li className="p-1 py-3 m-2 hover:rounded-lg hover:bg-gray-100 w-10">
+            <li
+              className={`p-1 py-3 m-2 hover:rounded-lg hover:bg-gray-100 w-10 ${
+                !Theme && "text-white hover:text-black"
+              }`}
+            >
               <FiSettings
                 style={{
                   margin: "2px",
@@ -250,7 +298,11 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
                 }}
               />
             </li>
-            <li className="p-1  m-2 hover:rounded-lg hover:bg-gray-100 flex w-10">
+            <li
+              className={`p-1  m-2 hover:rounded-lg hover:bg-gray-100 flex w-10 ${
+                !Theme && "text-white hover:text-black"
+              }`}
+            >
               <AiOutlineFlag
                 style={{
                   margin: "2px",
@@ -266,6 +318,7 @@ const SidebarVisibilty = ({ icons, visibilty }) => {
 };
 const Sidebar = () => {
   const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+  const Theme = useSelector((store) => store.app.Theme);
   const location = useLocation();
   const [search] = useSearchParams();
   let search1 = search?.get("v");
@@ -278,12 +331,12 @@ const Sidebar = () => {
       {isMenuOpen && location?.pathname === "/watch" && (
         // <div className="w-48 mt-4 ml-3 shadow-md ">
         <div
-          className={`w-48 mt-4    shadow-sm fixed top-0 left-0 bg-white h-full overflow-y-auto transition-transform transform ${
+          className={`w-48 mt-4  shadow-sm fixed top-0 left-0  h-full overflow-y-auto transition-transform transform ${
             isMenuOpen ? "translate-x-0" : "-translate-x-48"
-          } `}
+          } ${!Theme ? "bg-black" : "bg-white"}`}
           style={{ zIndex: 1000, marginTop: "60px" }}
         >
-          <SidebarVisibilty visibilty={search1} />
+          <SidebarVisibilty visibilty={search1} Theme = {Theme}/>
         </div>
       )}
       {isMenuOpen &&
@@ -293,14 +346,14 @@ const Sidebar = () => {
               isMenuOpen ? "fixed top-16 left-0" : "-translate-x-48"
             }`}
           >
-            <SidebarVisibilty visibilty={search1} />
+            <SidebarVisibilty visibilty={search1} Theme = {Theme}/>
           </div>
         )}
 
       {!isMenuOpen &&
       location.pathname !== "/watch" && ( //w-20 mt-4 ml-5
           <div className={`w-20 mt-2 ml-5 pt-16 fixed`}>
-            <SidebarVisibilty visibilty={search1} icons={"X"} />
+            <SidebarVisibilty visibilty={search1} icons={"X"} Theme = {Theme}/>
           </div>
         )}
     </>
