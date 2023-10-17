0e9f727b256c00f5d72183301242c2838695a797
Prajvals1211 <121630883+Prajvals1211@users.noreply.github.com>
2023-10-17 16:34:43 +0530

    Update SearchPage.js

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import { GOOGLE_API_KEY } from "../utils/constants";
 import SearchVideo from "./SearchVideo";
 import { useSelector } from "react-redux";
+import { SearchPageShimmer } from "./Shimmer";
 
 const SearchPage = () => {
   const { id } = useParams();
@@ -10,7 +11,6 @@ const SearchPage = () => {
   const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
   const Theme = useSelector((store) => store.app.Theme);
 
-  
   useEffect(() => {
     getSearchList();
   }, [id]);
@@ -25,15 +25,23 @@ const SearchPage = () => {
   };
 
   return (
-    <div className={`flex flex-col items-center  ${!isMenuOpen ? 'ml-[500px]' : 'ml-[500px]'} p-16`}>
-      {searchlist &&
-        searchlist.map((search) => (
-          <Link to={"/watch?v=" + search.id.videoId} key={search.id.videoId}>
-            <SearchVideo info={search} Theme = {Theme}/>
-          </Link>
-        ))}
-      {/* <SearchVideo info={searchlist[0]} /> */}
-    </div>
+    <>
+      
+      <div>{!searchlist.length && <SearchPageShimmer />}</div>
+      <div
+        className={`flex flex-col items-center  ${
+          !isMenuOpen ? "ml-[500px]" : "ml-[500px]"
+        } p-16`}
+      >
+        {searchlist &&
+          searchlist.map((search) => (
+            <Link to={"/watch?v=" + search.id.videoId} key={search.id.videoId}>
+              <SearchVideo info={search} Theme={Theme} />
+            </Link>
+          ))}
+        {/* <SearchVideo info={searchlist[0]} /> */}
+      </div>
+    </>
   );
 };
 
