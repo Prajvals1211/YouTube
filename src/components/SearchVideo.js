90debbdb06d738c264685bb9fe8f80b87f2c1a1e
Prajvals1211 <121630883+Prajvals1211@users.noreply.github.com>
2023-10-17 13:27:22 +0530

    Update SearchVideo.js

--- src/components/SearchVideo.js
+++ src/components/SearchVideo.js
@@ -1,13 +1,20 @@
 import React from "react";
 
-const SearchVideo = ({ info, flag }) => {
-  const { snippet } = info;
-  const { thumbnails, title, channelTitle } = snippet;
+const SearchVideo = ({ info, flag,Theme }) => {
+  const { snippet } = info;   //statistics
+  const { thumbnails, title, channelTitle } = snippet;  //publishedAt
   return (
+    // <div className="w-[500px]">
+    //   <div className="flex items-center ml-72" >
+    //   <img className="rounded-lg mx-auto" alt="thumbnail" src={thumbnails.medium.url} />
+    //     <p className="font-bold py-2">{title}</p>
+    //     <p className="font-mono">{channelTitle}</p>
+    //   </div>
+    // </div>
     <div
       className={`${
         flag === "X" ? "w-[350px] relative overflow-hidden" : "w-[500px]"
-      }  shadow-md rounded-lg overflow-hidden`}
+      }  shadow-md rounded-lg overflow-hidden ${!Theme ? "text-white hover:bg-gray-600" : "hover:bg-gray-100 text-black"}`}
     >
       <div className="flex p-4">
         <img
@@ -21,7 +28,7 @@ const SearchVideo = ({ info, flag }) => {
           <p className={`${flag === "X" ? "text-xs " : ""}font-bold`}>
             {title}
           </p>
-          <p className={`text-gray-600 ${flag === "X" ? "text-xs " : ""}`}>{channelTitle}</p>
+          <p className={`${!Theme ? "text-white" : "text-black"} ${flag === "X" ? "text-xs " : ""}`}>{channelTitle}</p>
         </div>
       </div>
     </div>
